32a7d5f317dc8f9f7e33b1537432238ac9a363ec
Daniel Gröber <darklord@darkboxed.org>
2011-08-31 17:39:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+SlShow
+======
+
+Clientside only HTML/JavaScript slideshow. Because I was too fucking lazy to
+install a serverside slideshow thingy.
+
+Installation
+------------
+
+scp slshow.html you@example.org:/var/www/yoursite/images/
+
+Done.
+
+Your server has to have indexing enabled in that directory tough. SlShow will
+try to fetch the index.html file on the clientside and find all the image files
+in that directory using a regular expression.
---
+++ slshow.js
@@ -0,0 +1,21 @@
+/*
+ * Clientside only HTML/JavaScript slideshow
+ * Copyright (C) 2011  Daniel Gröber <me ät dxld dot at>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$.ready(function(){
+    $.ajax()
+});
